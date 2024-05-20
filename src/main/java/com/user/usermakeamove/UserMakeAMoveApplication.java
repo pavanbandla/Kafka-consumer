fb4db246107d347f8f54eff99c8cb6312f79ea50
pawan Krishna <pawankrishna@pawans-MacBook-Pro.local>
2024-05-20 14:59:35 -0400

    kafka-consumer

---
+++ src/main/java/com/user/usermakeamove/UserMakeAMoveApplication.java
@@ -0,0 +1,13 @@
+package com.user.usermakeamove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMakeAMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMakeAMoveApplication.class, args);
+	}
+
+}
